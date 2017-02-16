8ba70bb48c23b59f0fae95e6fec3fc4b56fde4cc
ErickRyu <rkggmdii@gmail.com>
2017-02-16 22:05:59 +0900

    2017_02_16

---
+++ Soncoding/src/_2017_02_16/Base7.java
@@ -0,0 +1,37 @@
+package _2017_02_16;
+
+public class Base7 {
+	// Source : https://leetcode.com/problems/base-7/
+	// Author : ErickRyu
+	// Date   : 2017-02-16
+
+	/*
+	 * Given an integer, return its base 7 string representation.
+	 * 
+	 * Example 1:
+	 * Input: 100
+	 * Output: "202"
+	 * 
+	 * Example 2:
+	 * Input: -7
+	 * Output: "-10"
+	 * 
+	 * Note: The input will be in range of [-1e7, 1e7].
+	*/	
+	/**
+	 * Created on 2017/02/16
+	 */
+	public static String convertToBase7(int num) {
+		StringBuilder sb = new StringBuilder();
+		boolean isMinus = num < 0? true : false;
+		num = Math.abs(num);
+		do{
+			int remainder = num % 7;
+			sb.insert(0, remainder);
+			num /= 7;
+		}while(num > 0);
+		if(isMinus)
+			sb.insert(0, "-");
+		return sb.toString();
+	}
+}
---
+++ Soncoding/src/_2017_02_16/FindLargestValueInEachTreeRow.java
@@ -0,0 +1,57 @@
+package _2017_02_16;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class FindLargestValueInEachTreeRow {
+	// Source : https://leetcode.com/problems/find-largest-value-in-each-tree-row/?tab=Solutions
+	// Author : ErickRyu
+	// Date   : 2017-02-16
+
+	/*
+	 * You need to find the largest value in each row of a binary tree.
+	 * 
+	 * Example:
+	 * Input: 
+	 * 
+	 *           1
+	 *          / \
+	 *         3   2
+	 *        / \   \
+	 *       5   3   9 
+	 *       
+	 * Output: [1, 3, 9]
+	 */	
+	/**
+	 * Created on 2017/02/16
+	 */
+	
+    public List<Integer> largestValues(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        List<Integer> largestVals = new ArrayList<>();
+        if(root == null)return largestVals;
+        queue.add(root);
+        int queueSize = queue.size();
+        while(!queue.isEmpty()){
+        	int max = Integer.MIN_VALUE;
+        	for(int i = 0; i < queueSize; i++){
+        		TreeNode myNode = queue.poll();
+        		max = Math.max(max, myNode.val);
+        		if(myNode.left != null) queue.add(myNode.left);
+        		if(myNode.right != null) queue.add(myNode.right);
+        	}
+        	largestVals.add(max);
+        	queueSize = queue.size();
+        }
+        return largestVals;
+    }
+	
+    public class TreeNode {
+   	 int val;
+   	 TreeNode left;
+   	 TreeNode right;
+   	 TreeNode(int x) { val = x; }
+    }
+}
